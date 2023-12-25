9340397a16dfb6eed2193caf50219b356a4c0687
Mohamed Alaa <mohamedalaa4j@gmail.com>
2023-12-25 11:38:42 +0200

    Buttons added

--- app/src/main/java/com/example/jetpackcomposebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasicscodelab/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,16 +44,30 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Column(modifier = Modifier.padding(24.dp)) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+                .fillMaxWidth()
+        ) {
 
-            Text(
-                text = "Hello"
-            )
-            Text(
-                text = "$name!"
-            )
+            Column(modifier = Modifier.weight(1f)) {
+
+                Text(text = "Hello")
+                Text(text = "$name!")
+            }
+
+            OutlinedButton(onClick = { },
+                colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.white)
+                )
+            ) {
+                Text(text = "Show more", color = colorResource(id = R.color.purple_200))
+            }
         }
+
     }
 }
 
